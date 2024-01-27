22cc7f193dfe6c3aa917cdf1958a12fd2126e343
REDzHUB <138633493+REDzHUB@users.noreply.github.com>
2024-01-27 20:06:23 -0300

    Update Mobile.lua

--- Mobile.lua
+++ Mobile.lua
@@ -28,7 +28,7 @@ function Decompile:Type(part, Lines)
   local Script = "", ""
   
   if type == "table" then
-    Script, IsFirst = Script .. "{"
+    Script, IsFirst = Script .. "{", false
     
     for a,b in pairs(part) do
       if IsFirst then Script = Script .. ","end
@@ -42,7 +42,8 @@ function Decompile:Type(part, Lines)
       IsFirst = true
     end
     
-    Script = Script .. "\n" .. Lines .. "}"
+    local An = IsFirst and "\n" .. Lines or ""
+    Script = Script .. An .. "}"
   elseif type == "string" then
     Script = Script .. '"' .. part .. '"'
   elseif type == "Instance" then
@@ -66,7 +67,10 @@ function Decompile:Type(part, Lines)
           Variavel2 = Variavel2 .. ".LocalPlayer"
         elseif b == Player.Name and firstName == "workspace" then
           table.insert(Variaveis, "Players")
-          Variavel2 = Variavel2 .. "[Players.LocalPlayer.Name]"
+          Variavel2, firstName = "Players.LocalPlayer.Character", ""
+        elseif b:find(Player.Name) and firstName == "workspace" then
+          table.insert(Variaveis, "Players")
+          Variavel2 = Variavel2 .. "\[Player.Name .. \"" .. b:gsub(Players.LocalPlayer.Name, "") .. "\"\]"
         elseif b == "Camera" and firstName == "workspace" then
           Variavel2 = Variavel2 .. ".CurrentCamera"
         elseif b:find(" ")
@@ -96,9 +100,9 @@ function Decompile:Type(part, Lines)
   elseif type == "function" then
     Script = Script .. "function(" .. GetParams(part) .. ")"
     if Decompile.getupvalues then
-      local upvalue, constant
+      local Count, upvalue, constant = false
       pcall(function()
-        if getupvalues and #getupvalues(part) >= 0 then
+        if getupvalues and #getupvalues(part) >= 0 then vals = true
           for a,b in pairs(getupvalues(part)) do Wait()
             
             if not upvalue then Script = Script .. "\n" .. Lines .. "  local upvalues = {\n" .. Lines .. "    "
@@ -112,9 +116,8 @@ function Decompile:Type(part, Lines)
     end
     if Decompile.getconstants then
       pcall(function()
-        if getconstants and #getconstants(part) > 0 then
+        if getconstants and #getconstants(part) > 0 then vals = true
           for a,b in pairs(getconstants(part)) do Wait()
-            
             if not constant then Script = Script .. "\n" .. Lines .. "  local constants = {\n" .. Lines .. "    "
             else Script = Script .. ",\n" .. Lines .. "    " end
             Script = Script .. "[" .. a .. "] = " .. Decompile:Type(b, Lines .. "    ")
@@ -124,7 +127,8 @@ function Decompile:Type(part, Lines)
         end
       end)
     end
-    Script = Script .. "\n" .. Lines .. "end"
+    local An = vals and "\n" .. Lines or ""
+    Script = Script .. An .. "end"
   elseif type == "Vector3" then
     Script = Script .. "Vector3.new(" .. tostring(part) .. ")"
   elseif type == "Color3" then
@@ -184,9 +188,11 @@ function Decompile.new(part)
     Script = Script .. "\n" .. Lines .. "[\"1\"] = " .. Decompile:Type(GetClass(part), Lines)
   end
   
-  local Var = ""
+  local Var, list = "", {}
   table.foreach(Variaveis, function(_,Val)
+    if table.find(list, Val) then return end
     Var = Var .. "local " .. Val .. "\ = game:GetService(\"" .. Val .. "\")\n"
+    table.insert(list, Val)
   end)
   
   if Decompile.setclipboard then
